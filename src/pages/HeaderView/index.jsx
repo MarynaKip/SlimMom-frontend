dc84ca71efd6466228bf2666024dfaa3ced09739
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-24 20:27:18 +0300

    Created HeaaderView page component

---
+++ src/pages/HeaderView/index.jsx
@@ -0,0 +1,13 @@
+import Container from '../../components/ContainerForHeader';
+import Header from '../../components/Header';
+import './styles.css';
+const HeaderView = () => {
+    return (
+        <div className='underline'>
+        <Container>
+            <Header />
+        </Container>
+        </div>)
+   
+}
+export default HeaderView;
