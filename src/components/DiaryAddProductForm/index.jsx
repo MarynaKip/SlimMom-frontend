b39e8c72eb10fb8d199a6b1932fb32a4373553f1
Olga Verbetska <verbetska150@gmail.com>
2021-07-25 17:23:39 +0300

    add formik

--- src/components/DiaryAddProductForm/index.jsx
+++ src/components/DiaryAddProductForm/index.jsx
@@ -1,2 +1,47 @@
-const DiaryAddProductForm = () => {return (<p>Это форма добавить продукт. Отображается только в сегодня</p>)}
+
+import React from 'react';
+import { useFormik } from 'formik';
+
+const DiaryAddProductForm = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const formik = useFormik({
+    initialValues: {
+      productName: '',
+      grams:Number,
+    },
+    onSubmit: values => {
+      //понять что значит эта строка
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (<div>
+    <p>Это форма добавить продукт. Отображается только в сегодня</p>
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        id="productName"
+        name="productName"
+        type="productName"
+        onChange={formik.handleChange}
+        value={formik.values.productName}
+        placeholder = "Введите название продукта"
+        required
+      />
+       <input
+        id="grams"
+        name="grams"
+        type="grams"
+        onChange={formik.handleChange}
+        value={formik.values.grams}
+        placeholder = "Граммы"
+        required
+      />
+
+
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  );
+};
+
 export default DiaryAddProductForm;
--- src/components/DiaryDateСalendar/index.jsx
+++ src/components/DiaryDateСalendar/index.jsx
@@ -1,3 +1,21 @@
-const DiaryDateСalendar = () => {return <p>Это календарик</p>}
+import DatePicker from 'react-date-picker';
+import styles from '../DiaryDateСalendar/DiaryDateСalendar.module.css'
+import CalendarIcon from '../DiaryDateСalendar/CalendarIcon.jsx'
+import '../../index.css';
+
+const dateValue = Date.now()
+const DiaryDateСalendar = () => {
+  return (
+  <>
+  <DatePicker
+  className = {styles.calendar}
+   value={dateValue}
+   format = "dd.MM.yyyy"
+   onChange = {null}
+   clearIcon = {null}
+   calendarIcon = {<CalendarIcon/>}
+/>
+  </>)
+};
 
 export default DiaryDateСalendar;
