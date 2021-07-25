cc484f02de7d1a1a2a6f7736d4cd537a2aaf4631
Olga Verbetska <verbetska150@gmail.com>
2021-07-25 20:40:16 +0300

    add product form styles

--- src/components/DiaryAddProductForm/index.jsx
+++ src/components/DiaryAddProductForm/index.jsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import { useFormik } from 'formik';
+import styles from '../DiaryAddProductForm/DiaryAddProductForm.module.css'
 
 const DiaryAddProductForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
@@ -8,38 +8,55 @@ const DiaryAddProductForm = () => {
   const formik = useFormik({
     initialValues: {
       productName: '',
-      grams:Number,
+      grams: Number,
     },
-    onSubmit: values => {
+    onSubmit: (values) => {
       //понять что значит эта строка
       alert(JSON.stringify(values, null, 2));
     },
   });
-  return (<div>
-    <p>Это форма добавить продукт. Отображается только в сегодня</p>
-    <form onSubmit={formik.handleSubmit}>
-      <input
-        id="productName"
-        name="productName"
-        type="productName"
-        onChange={formik.handleChange}
-        value={formik.values.productName}
-        placeholder = "Введите название продукта"
-        required
-      />
-       <input
-        id="grams"
-        name="grams"
-        type="grams"
-        onChange={formik.handleChange}
-        value={formik.values.grams}
-        placeholder = "Граммы"
-        required
-      />
+  return (
+    <div>
+          <form onSubmit={formik.handleSubmit} className = {styles.addProductForm}>
+        <input
+          className = {styles.inputAddProductFormName}
+          id='productName'
+          name='productName'
+          type='productName'
+          onChange={formik.handleChange}
+          value={formik.values.productName}
+          placeholder='Введите название продукта'
+          required
+          list='products-for-add'
+          autocomplete='off'
+        />
 
+        <datalist id='products-for-add'>
+          <option value=''></option>
+          <option>gre4ka</option>
+          <option value='jaico'></option>
+          <option value='salo'></option>
+          <option value='moloko'></option>
+        </datalist>
+ {/* <Field name="color" component="select">
+   <option value="red">Red</option>
+   <option value="green">Green</option>
+   <option value="blue">Blue</option>
+ </Field> */}
+        <input
+               className = {styles.inputAddProductFormAmount}
+          id='grams'
+          name='grams'
+          type='grams'
+          onChange={formik.handleChange}
+          value={formik.values.grams}
+          placeholder='Граммы'
+          required
+          autocomplete='off'
+        />
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type='submit' className = {styles.buttonAddProduct}>+</button>
+      </form>
     </div>
   );
 };
