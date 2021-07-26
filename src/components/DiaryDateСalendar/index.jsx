7a10934288774d85ef26797ee6dafd3d87314dd5
Olga Verbetska <verbetska150@gmail.com>
2021-07-26 16:10:01 +0300

    calendar

--- src/components/DiaryDateСalendar/index.jsx
+++ src/components/DiaryDateСalendar/index.jsx
@@ -1,21 +1,31 @@
 import DatePicker from 'react-date-picker';
 import styles from '../DiaryDateСalendar/DiaryDateСalendar.module.css'
-import CalendarIcon from '../DiaryDateСalendar/CalendarIcon.jsx'
-import '../../index.css';
+import CalendarIcon from '../DiaryDateСalendar/CalendarIcon.jsx';
+// import {useState} from "react-redux";
+import React, { useState } from 'react';
 
-const dateValue = Date.now()
-const DiaryDateСalendar = () => {
+
+
+import '../../index.css';
+// const dateValue = Date.now()
+
+console.log(useState)
+export default function DiaryDateСalendar() {
+
+
+const [value, onChange] = useState (new Date());
+
+
   return (
   <>
   <DatePicker
   className = {styles.calendar}
-   value={dateValue}
+   value={ value}
    format = "dd.MM.yyyy"
-   onChange = {null}
+   onChange = {onChange}
    clearIcon = {null}
    calendarIcon = {<CalendarIcon/>}
 />
-  </>)
+  </>);
 };
 
-export default DiaryDateСalendar;
