d43d2d2745ee49b947c91cea99b53caf7d06bcbc
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-30 19:45:41 +0300

    template diary types

---
+++ src/redux/diaryData/contacts-types.js
@@ -0,0 +1,5 @@
+const ADD_CONTACT = 'form/Add_contact';
+const DELETE_CONTACT = 'contactList/Delete_contact';
+const FILTER_CONTACT = 'findInput/Filter_change';
+
+export default {ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACT};
