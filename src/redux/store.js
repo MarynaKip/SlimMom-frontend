7895a38996064ec06e5274827df9d24b613d9e0c
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 12:13:26 +0300

    connect Redux DevTools and perssistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,42 +1,48 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import process from 'process';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
+import contactsReducer from '../redux/diaryData/contacts-reducer';
+import { authReducer } from './auth';
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+const devtols =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const middleware = [
-    ...getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    }),
-    // logger,
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
 ];
 
 const authPersistConfig = {
-    key: 'auth',
-    storage,
-    whitelist: ['token'],
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
 };
 
 const store = configureStore({
-    reducer: {
-        user: null,
-        auth: persistReducer(authPersistConfig, null),
-    },
-    middleware,
-    devTools: process.env.NODE_ENV === 'development',
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+  },
+  middleware,
+  devtols,
+  //   devTools: process.env.NODE_ENV === 'development',
 });
 
 const persistor = persistStore(store);
 
-export { store, persistor };
+export default { store, persistor };
