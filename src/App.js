3334056cb600cd4025dd21d2937154200245cb4b
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 12:15:49 +0300

    added localstorage get current user

--- src/App.js
+++ src/App.js
@@ -8,30 +8,41 @@ import PromoView from './pages/PromoView';
 import DiaryPageView from './pages/DiaryPageView';
 import RegisterLoginPageView from './pages/RegisterLoginPageView';
 
-const App = () => {
-    return (
-        <>
-            <Switch>
-                <PublicRoute exact path={routes.home} component={PromoView} />
-                <PublicRoute
-                    path={routes.register}
-                    restricted
-                    redirectTo={routes.mydiary}
-                    component={RegisterLoginPageView}
-                />
-                <PublicRoute
-                    path={routes.login}
-                    restricted
-                    redirectTo={routes.mydiary}
-                    component={RegisterLoginPageView}
-                />
-                <PublicRoute path={routes.mydiary} component={DiaryPageView} />
-                {/* <PrivateRoute path={routes.calculator} render={Calculator}/> */}
-                {/* {modal && <Modal />} */}
-                <Redirect to="/" />
-            </Switch>
-        </>
-    );
+import { connect } from 'react-redux';
+import { authOperations } from './redux/auth';
+import { useEffect, Suspense, lazy } from 'react';
+
+const App = ({ onGetCurrentUser }) => {
+  useEffect(() => {
+    onGetCurrentUser();
+  }, []);
+  return (
+    <>
+      <Switch>
+        <PublicRoute exact path={routes.home} component={PromoView} />
+        <PublicRoute
+          path={routes.register}
+          restricted
+          redirectTo={routes.mydiary}
+          component={RegisterLoginPageView}
+        />
+        <PublicRoute
+          path={routes.login}
+          restricted
+          redirectTo={routes.mydiary}
+          component={RegisterLoginPageView}
+        />
+        <PublicRoute path={routes.mydiary} component={DiaryPageView} />
+        {/* <PrivateRoute path={routes.calculator} render={Calculator}/> */}
+        {/* {modal && <Modal />} */}
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
 };
 
-export default App;
+const mapDispatchToProps = {
+  onGetCurrentUser: authOperations.getCurrentUser,
+};
+
+export default connect(null, mapDispatchToProps)(App);
