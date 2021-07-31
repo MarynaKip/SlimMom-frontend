9eec78112cdef2d34ede616a263991063d5f213c
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 12:19:35 +0300

    remove space

--- src/components/ContainerForLogin/index.jsx
+++ src/components/ContainerForLogin/index.jsx
@@ -1,11 +1,11 @@
 import styles from './styles.module.css';
 
 const ContainerForView = ({children}) => {
-    return (
-        <div className={styles.wrapper}>
-            { children }
-        </div>
-    );
+  return (
+    <div className={styles.wrapper}>
+      { children }
+    </div>
+  );
 };
 
 export default ContainerForView;
