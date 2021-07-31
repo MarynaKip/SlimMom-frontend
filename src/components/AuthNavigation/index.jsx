e5b5d4a4a677e3e8e54551e5b587e2bda53829d1
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 12:20:00 +0300

    remove space

--- src/components/AuthNavigation/index.jsx
+++ src/components/AuthNavigation/index.jsx
@@ -1,26 +1,26 @@
 import { NavLink } from 'react-router-dom';
 import './styles.css';
 const AuthNavigation = () => {
-    return (
-        <div className="navigation_container">
-            <NavLink
-                to="/login"
-                exact
-                className="link"
-                activeClassName="active_link"
-            >
+  return (
+    <div className="navigation_container">
+      <NavLink
+        to="/login"
+        exact
+        className="link"
+        activeClassName="active_link"
+      >
                 ВХОД
-            </NavLink>
-            <NavLink
-                to="/register"
-                exact
-                className="link"
-                activeClassName="active_link"
-            >
+      </NavLink>
+      <NavLink
+        to="/register"
+        exact
+        className="link"
+        activeClassName="active_link"
+      >
                 РЕГИСТРАЦИЯ
-            </NavLink>
-        </div>
-    );
+      </NavLink>
+    </div>
+  );
 };
 
 export default AuthNavigation;
