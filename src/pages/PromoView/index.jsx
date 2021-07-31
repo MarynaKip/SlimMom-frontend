83d00da2adffc90e5a079790d6bbfda24a32a07d
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 19:51:43 +0300

    space remove

--- src/pages/PromoView/index.jsx
+++ src/pages/PromoView/index.jsx
@@ -2,12 +2,12 @@ import './styles.css';
 import Login from '../../components/LoginForm';
 import Header from '../../components/Header';
 
-const PromoView = () => {
-    return (
-        <main className="bg-wrapper__promo">
-            <Header />
-            <Login />
-        </main>
-    );
+const PromoView = (props) => {
+  return (
+    <main className="bg-wrapper__promo">
+      <Header />
+      <Login />
+    </main>
+  );
 };
 export default PromoView;
