7fb230336ead65ebdce82c9458c99201fb95a45e
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-07-31 20:00:32 +0300

    PrivateRoute realised isAuthenticate

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,6 +1,6 @@
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-// import { authSelectors } from '../../redux/auth';
+import { authSelectors } from '../../redux/auth';
 
 const PrivateRoute = ({
   component: Component,
@@ -11,18 +11,14 @@ const PrivateRoute = ({
   <Route
     {...routeProps}
     render={props =>
-      isAuthenticated ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={redirectTo} />
-      )
+      isAuthenticated ? <Component {...props} /> : <Redirect to={redirectTo} />
     }
   />
 );
 
-// const mapStateToProps = state => ({
-//     isAuthenticated: authSelectors.getIsLoggedIn(state),
-// });
+const mapStateToProps = state => ({
+  isAuthenticated: authSelectors.getIsAuthenticated(state),
+});
 
-export default PrivateRoute;
-// export default connect(mapStateToProps)(PrivateRoute);
+// export default PrivateRoute;
+export default connect(mapStateToProps)(PrivateRoute);
